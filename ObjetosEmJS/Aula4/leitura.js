5fd8a49080cc33898272503d9c63fac064ddf4b8
Lsteixeira <lucas.s.teixeira@viannasempre.com.br>
2023-01-31 10:45:18 -0300

    Trabalhando com JSON e String

--- ObjetosEmJS/Aula4/leitura.js
+++ ObjetosEmJS/Aula4/leitura.js
@@ -1,3 +1,11 @@
 const dados = require("./cliente.json");
 
-console.log(dados);
+//console.log(dados);
+
+const clienteEmString = JSON.stringify(dados);
+
+//console.log(clienteEmString);
+
+// Transformando string em objeto
+
+console.log(JSON.parse(clienteEmString));
